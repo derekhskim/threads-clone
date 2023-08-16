128dcb8b1a15f8cfe60d8ab7d9bf4df6cbdcae19
[treasure3210] <treasure3210@gmail.com>
2023-08-15 21:26:00 -0600

    Initial Commit

---
+++ threads-clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  threads-clone
+//
+//  Created by Derek Kim on 2023-08-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ threads-clone/threads_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  threads_cloneApp.swift
+//  threads-clone
+//
+//  Created by Derek Kim on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct threads_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
