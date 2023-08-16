f9879a5f8472fa80bdbb3773b402723bb0425cf5
[treasure3210] <treasure3210@gmail.com>
2023-08-15 21:28:46 -0600

    Setup basic folder structure

---
+++ threads-clone/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  threads-clone
+//
+//  Created by Derek Kim on 2023-08-15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
