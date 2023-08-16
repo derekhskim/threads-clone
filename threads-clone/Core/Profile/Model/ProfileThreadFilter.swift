b3bf575f26c72883f7051939b090b281faf03420
[treasure3210] <treasure3210@gmail.com>
2023-08-16 15:26:03 -0600

    Upper UI configuration for profileview

---
+++ threads-clone/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileThreadFilter.swift
+//  threads-clone
+//
+//  Created by Derek Kim on 2023-08-16.
+//
+
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+    case threads
+    case replies
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+}
